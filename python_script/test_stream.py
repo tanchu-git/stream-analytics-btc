2e22b9dd04782c5adf1177566bcf5ebd455b7aee
Tan Chu <139019601+tanchu-git@users.noreply.github.com>
2023-09-13 00:02:07 +0200

    Update test_stream.py

--- python_script/test_stream.py
+++ python_script/test_stream.py
@@ -1,7 +1,8 @@
 import time
 import json
 import pytest
-
+import btc_stream
+    
 # pip install binance-connector
 from binance.websocket.spot.websocket_stream import SpotWebsocketStreamClient
 
@@ -25,28 +26,23 @@ def stream_messages():
 
     return messages, raw_messages
 
-@pytest.fixture()
-def new_keys():
-    """
-    Return new key names for dictionary.
-    """
-    return [
-        "Type",
-        "Event Time",
-        "Symbol",
-        "Trade ID",
-        "Price",
-        "Quantity",
-        "Buyer order ID",
-        "Seller Order ID",
-        "Trade time",
-        "Market Maker",
-        "Ignore"
-    ]
-
 EXPECTED_ITEMS_COUNT = 11
 EXPECTED_DICT = dict
 EXPECTED_STR = str
+
+def test_rename_keys(stream_messages):
+    messages, _ = stream_messages
+    expected_keys = btc_stream.new_keys
+
+    # Skip first message.
+    for msg in messages[1:]:
+        expected_values = str(msg.values())
+        renamed = btc_stream.rename_keys(btc_stream.new_keys, msg)
+
+        assert type(renamed) == EXPECTED_DICT
+        assert len(renamed) == EXPECTED_ITEMS_COUNT
+        assert list(renamed.keys()) == expected_keys
+        assert str(renamed.values()) == expected_values
 
 def test_first_message(stream_messages):
     messages, _ = stream_messages
@@ -62,20 +58,6 @@ def test_remaining_messages(stream_messages):
     for msg in messages[1:]:
         assert type(msg) == EXPECTED_DICT
         assert len(msg) == EXPECTED_ITEMS_COUNT
-
-def test_rename_keys(new_keys, stream_messages):
-    messages, _ = stream_messages
-    expected_keys = new_keys
-    assert len(messages) > 1
-
-    # Skip first message.
-    for msg in messages[1:]:
-        expected_values = str(msg.values())
-        zipped_dict = dict(zip(new_keys, msg.values()))
-
-        assert len(zipped_dict) == EXPECTED_ITEMS_COUNT
-        assert list(zipped_dict.keys()) == expected_keys
-        assert str(zipped_dict.values()) == expected_values
 
 def test_raw_message(stream_messages):
     _, raw_messages = stream_messages
