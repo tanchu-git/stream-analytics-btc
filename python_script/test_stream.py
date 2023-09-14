bc7abd96ad056e90783ec922df878e00ca0a2ff2
Tan Chu <139019601+tanchu-git@users.noreply.github.com>
2023-09-14 23:50:24 +0200

    Update test_stream.py

--- python_script/test_stream.py
+++ python_script/test_stream.py
@@ -2,64 +2,30 @@ import time
 import json
 import pytest
 import btc_stream
-    
-# pip install binance-connector
-from binance.websocket.spot.websocket_stream import SpotWebsocketStreamClient
 
 @pytest.fixture(scope="session")
-def stream_messages():
-    """
-    Start a connection to websocket stream.
-    Return stream messages (formatted and raw) in lists to test.
-    """
-    messages = []
-    raw_messages = []
-
-    def process_message(_, message):
-        raw_messages.append(message)
-        messages.append(json.loads(message))
-
-    stream = SpotWebsocketStreamClient(on_message=process_message)
-    stream.trade(symbol="btcusdt")
-    time.sleep(2)
-    stream.stop()
-
-    return messages, raw_messages
+def stream_message():
+    return ('{"e":"trade","E":1694726183301,"s":"BTCUSDT","t":3212504520,"p":"26596.52000000",'
+            '"q":"0.00380000","b":22343123975,"a":22343123438,"T":1694726183301,"m":false,"M":true}')
 
 EXPECTED_ITEMS_COUNT = 11
 EXPECTED_DICT = dict
 EXPECTED_STR = str
 
-def test_rename_keys(stream_messages):
-    messages, _ = stream_messages
+def test_process_message(monkeypatch, stream_message):
     expected_keys = btc_stream.new_keys
 
-    # Skip first message.
-    for msg in messages[1:]:
-        expected_values = str(msg.values())
-        renamed = btc_stream.rename_keys(btc_stream.new_keys, msg)
+    def fake_process_message(on_message):        
+        string_to_dict = json.loads(on_message)
+        dict_renamed = btc_stream.rename_keys(btc_stream.new_keys, string_to_dict)        
+        return dict_renamed
 
-        assert type(renamed) == EXPECTED_DICT
-        assert len(renamed) == EXPECTED_ITEMS_COUNT
-        assert list(renamed.keys()) == expected_keys
-        assert str(renamed.values()) == expected_values
+    monkeypatch.setattr(btc_stream, "process_message", fake_process_message)
+    test_dict = btc_stream.process_message(stream_message)
+    expected_values = str(test_dict.values())
 
-def test_first_message(stream_messages):
-    messages, _ = stream_messages
-    first_message = messages[0]    
-    assert type(first_message) == EXPECTED_DICT
-    assert len(first_message) == 2
-
-def test_remaining_messages(stream_messages):
-    messages, _ = stream_messages
-    assert len(messages) > 1
-
-    # Skip first message.
-    for msg in messages[1:]:
-        assert type(msg) == EXPECTED_DICT
-        assert len(msg) == EXPECTED_ITEMS_COUNT
-
-def test_raw_message(stream_messages):
-    _, raw_messages = stream_messages
-    for msg in raw_messages:
-        assert type(msg) == EXPECTED_STR
+    assert type(stream_message) == EXPECTED_STR
+    assert type(test_dict) == EXPECTED_DICT
+    assert len(test_dict) == EXPECTED_ITEMS_COUNT
+    assert list(test_dict.keys()) == expected_keys
+    assert str(test_dict.values()) == expected_values
